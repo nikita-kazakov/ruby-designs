92018a99259e5e47879f8eeecc154b06c58ebec6
Nikita Kazakov <contactnikitakazakov@gmail.com>
2019-03-19 17:12:08 -0400

    East Principle Practice with Objects.

--- input_output.rb
+++ input_output.rb
@@ -71,4 +71,162 @@ end
 
 
 personobject.do_speak
+puts "\n\n\n"
 
+
+#Passing Arguments to Objects
+p [1,2,3,4,5].include?(5) # false
+p Array.new(4, "Dude") # ["Dude", "Dude", "Dude", "Dude"]
+p (3 + 2) #5
+
+#Realize that operators are simply object LITERALS
+
+puts 10 + 5  # 15. That plus is literal.
+puts 10.+(5) # 15. That plus is really a method that's taking an integer as a parameter.
+
+#Don't be afraid.  You can create your own Add10 Method.
+class Add10
+  def +(number)
+    10 + number
+  end
+end
+
+add10 = Add10.new
+puts add10.+(5)
+
+#Let's write a message that accepts a string, waits, and outputs a lowercase of it.
+#Let's make an outline of how we want to structure it first:
+#We're not worried about where messages will be stored right now... Keep it simple.
+#Step 1 -->
+def slowercase(a_string)
+  #TODO: sleep for 3 seconds
+  #TODO: return a lowercase version of the string
+  a_string
+end
+
+puts slowercase("CoOlStUFf")
+
+#Step 2 --> Make it sleep for 2 seconds.
+def slowercase(a_string)
+  sleep 2
+  #TODO: return a lowercase version of the string
+  a_string
+end
+
+#Step 3 --> Apply lowercase method.
+def slowercase(a_string)
+  #sleep 2
+  a_string.downcase
+end
+
+puts slowercase("CoOlStUFf")
+
+puts "\n\n\n"
+
+#Polymorphism allows us to send messages with minimal coupling.  This is called Duck Typing in dynamic languages.
+#You have the ability for multiple objects to respond to ONE message in DIFFERENT WAYS.
+#Let's look an an example of POLYMORPHISM.
+
+def print_a_line(printer, string)
+  printer.print_line string
+end
+
+class PrintPrinter
+  def print_line(string)
+    print(string + "\n")
+  end
+end
+
+class PutsPrinter
+  def print_line(string)
+    puts string
+  end
+end
+
+print_a_line(PrintPrinter.new, "Hi There")
+print_a_line(PutsPrinter.new, "Hi There")
+
+#So you're passing OBJECTS to the print_a_line definition.
+#Then calling a print_line method...both those objects have the PRINT LINE method available.
+
+#Let's revisit and make Slowercase Polymorphic.
+#Here's the original code:
+def slowercase(a_string)
+  sleep 0.5
+  a_string.downcase
+end
+
+#Remember, a method should have 1 responsibility.  This method has 2.  Sleep and downcase.
+#Let's create a class for it.
+
+class Slowercase
+
+  def initialize(slower, caser)
+    @slower = slower
+    @caser = caser
+  end
+
+  def slowercase(a_string)
+    @slower.slow
+    @caser.lowercase a_string
+  end
+
+end
+
+class Slower
+  def slow
+    sleep 1
+  end
+end
+
+class Lowercaser
+  def lowercase(a_string)
+    a_string.downcase
+  end
+end
+
+puts Slowercase.new(Slower.new, Lowercaser.new).slowercase("FuNkYCASe")
+
+#That's a lot to take in.
+
+
+
+#Skip to the East Method for OO.
+require 'set'
+
+class RubySteps
+
+  def initialize(emailer)
+    @emailer = emailer
+    @users = Set.new
+  end
+
+  def add_user(user)
+    @users.add(user)
+    user.deliver_welcome_email(@emailer)
+    @users.each {|u| u.member_registered(user, @emailer) }
+    true
+  end
+
+end
+
+class User
+  attr_reader :name
+
+  def initialize(name, email)
+    @name = name
+    @email = email
+    @friends = Set.new
+  end
+
+  def deliver_welcome_email(emailer)
+    emailer.deliver_email(
+        subject: "Welcome to RubySteps, #{@name}!",
+        to_email: @email,
+        to_name: @name,
+        body: "This is gonna be so much fun"
+    )
+    true
+  end
+
+end
