43f050ab107601683483bca75c93ab2fd279fcf9
Your Name <contactnikitakazakov@gmail.com>
2020-01-08 10:46:30 -0700

    Javascript ES6 Practice - conditionals, switches, if statements.

--- javascript_practice/vendor/js_4_loops.js
+++ javascript_practice/vendor/js_4_loops.js
@@ -69,3 +69,69 @@ const todoFilterText = todos.filter(function(item){
 console.log(todoFilterText)    // Only 2 objects returned!
 
 // on 45 minutes...
+
+// CONDITIONALS
+// If Statements
+
+const numx = 10
+
+if (numx==10){
+    console.log("X is 10!")
+}
+
+// Equal signs
+// When using ==, it doesn't match data types. It just matches values.
+// For example, if I change the numx to a string but check for number it will still be true. Bad.
+const numy = 10
+
+if (numy == 10) {
+    console.log('double equals...doesnt match data types unfortunately.')
+}
+
+// However, when you use ===, it matches data type AND value. Use ===.
+
+if (numy === 11) {
+    console.log('shows if 11.')
+}
+else{
+    console.log('not 11.')      // not 11.
+}
+
+// You can also use Else if.
+
+if (numy === 20){
+    console.log('shows if 11.')
+}
+else if (numy === 10){
+    console.log('shows if 10.')     //shows if 10.
+}
+else{
+    console.log('not 11.')
+}
+
+// You can also use && or ||
+if (numy < 20 && numy > 9){
+    console.log('its less than 20 and greater than 9')
+}
+
+
+// Ternary Operator
+const cnum = 10;
+const color = cnum > 10 ? 'red' : 'blue'
+console.log(cnum)       // 10
+
+
+// Switches - Same Idea as in Ruby. The default is like else fallthrough. You do need breaks for some reason.
+
+switch(color){
+    case 'red':
+        console.log('color is red')
+        break
+    case 'blue':
+        console.log('color is blue')
+        break
+    default:
+        console.log('doesnt match anything')
+        break
+}
+
